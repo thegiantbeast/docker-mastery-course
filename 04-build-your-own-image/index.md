068f2cac3d75173a1e4f043fdb606cc743051f3a
Ricardo Ferreira <thegiantbeast.pt@gmail.com>
2017-06-13 01:18:39 +0200

    add assignment for: build your own image

---
+++ 04-build-your-own-image/index.md
@@ -0,0 +1,8 @@
+# Assignment: Build Your Own Image
+
+* take existing Node.js app and Dockerize it
+* make `Dockerfile`. Build it. Test it. Push it. (rm it). Run it.
+* use the Alpine version of the official `node` 6.x image
+* expected result is web site at `http://localhost`
+* tag and push to your Docker Hub account (free)
+* remove your image from local cache, run again from Hub
---
+++ 04-build-your-own-image/run.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+username=thegiantbeast
+imageName=$username/own-image
+containerName=own-image-container
+
+startAndFetchContainer() {
+	docker container run --rm --env PORT=80 --name $containerName -d $imageName &> /dev/null
+	sleep 5
+
+	echo "Fetching app"
+	containerIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $containerName)
+	docker container run --rm -it byrnedo/alpine-curl $containerIP
+
+	echo "Stopping containers"
+	docker container stop $containerName > /dev/null
+}
+
+echo "Building image"
+docker image build --no-cache -t $imageName . > /dev/null
+
+echo "Running app container"
+startAndFetchContainer
+
+echo "Pushing image"
+docker image push $imageName > /dev/null
+
+echo "Removing local image"
+docker image rm $imageName > /dev/null
+
+echo "Running app container (from Docker Hub)"
+startAndFetchContainer
